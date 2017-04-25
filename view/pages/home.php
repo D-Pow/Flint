37c87f1b1f2da25912c2527845286fa20d3732d9
Devon <djp9373@gmail.com>
2017-04-24 21:32:15 -0400

    Finished the basis of the home page view. Made it easier to login.

--- view/pages/home.php
+++ view/pages/home.php
@@ -8,74 +8,171 @@
         <?php
     }
 
-    //projects should be listed by most recent date, and will be sorted
-    //by being before/after the last_login time
-    //$sortedProjects
-    //$sortedCompProjects
+    /**
+     * Gets the appropriate project content and converts
+     * it to a string containing the appropriate html
+     */
+    function displayProject($project) {
+        $html = "
+            <div class='entry'>
+                <h4>
+                    <a class='entry-title' 
+                        href='/Flint/?controller=pages&action=user&user="
+                        . $project->username
+                        . "'>"
+                        . $project->username . "
+                    </a>
+                    posted a new project: 
+                    <a class='entry-title' 
+                        href='/Flint/?controller=pages&action=project&pid="
+                        . $project->pid
+                        . "'>"
+                        . $project->pname ."
+                    </a>
+                </h4>
+                <p>"
+                . $project->description . "
+                </p>
+            </div>
+            ";
+        return $html;
+    }
 
+    /**
+     * Gets the appropriate donation content and converts
+     * it to a string containing the appropriate html
+     */
+    function displayDonation($donation) {
+        $html = "
+            <div class='entry'>
+                <br />
+                <p>
+                    <a class='entry-title' 
+                        href='/Flint/?controller=pages&action=user&user="
+                        . $donation->username
+                        . "'>"
+                        . $donation->username . "
+                    </a>
+                    pledged $"
+                    . $donation->amount
+                    . " towards 
+                    <a class='entry-title' 
+                        href='/Flint/?controller=pages&action=project&pid="
+                        .  $donation->pid
+                        . "'>"
+                        . $donation->pname . "
+                    </a>
+                    .
+                </p>
+            </div>
+            ";
+        return $html;
+    }
+
+    /**
+     * Gets the appropriate post content and converts
+     * it to a string containing the appropriate html
+     */
+    function displayPost($post) {
+        $html = "
+            <div class='entry'>
+                <h4>
+                    <a class='entry-title' 
+                        href='/Flint/?controller=pages&action=user&user="
+                        . $post->author
+                        . "'>"
+                        . $post->author . "
+                    </a>
+                    > 
+                    <a class='entry-title' 
+                        href='/Flint/?controller=pages&action=project&pid="
+                        .  $post->pid
+                        . "'>"
+                        . (
+                            ($post->owner)
+                            ? $post->pname . ' (Update)'
+                            : $post->pname 
+                          ) . "
+                    </a>
+                </h4>
+                <p>"
+                . $post->comment . "
+                </p>
+            </div>
+            ";
+        return $html;
+    }
 ?>
 
 <!--Header content-->
 <h2 id='user-title'>Welcome <?php echo $username; ?>!</h2>
 <br /><br />
-<a id='edit-profile' href='/Flint/?controller=pages&action=user&author=<?php 
+<a id='edit-profile' href='/Flint/?controller=pages&action=user&user=<?php 
     echo $username; ?>'>Profile</a>
 
 <?php
+    //arrays to hold the html to display
+    //will be shuffled so that different types of content are mixed together
+    $newContent = [];
+
+    //projects
+    if ($sortedProjects[0]) {
+        foreach ($sortedProjects[0] as $project) {
+            $newContent[] = displayProject($project);
+        }
+    }
+    
+    //donations
+    if ($sortedDonations[0]) {
+        foreach ($sortedDonations[0] as $donation) {
+            $newContent[] = displayDonation($donation);
+        }
+    }
+
+    //posts
+    if ($sortedPosts[0]) {
+        foreach ($sortedPosts[0] as $post) {
+            $newContent[] = displayPost($post);
+        }
+    }
+
+    shuffle($newContent);
+    foreach ($newContent as $html) {
+        echo $html;
+    }
+?>
+
+<p class='divider'>
+------------------------------------last login------------------------------------
+</p>
+
+<?php
+    $oldContent = [];
+
+    //projects
+    if ($sortedProjects[1]) {
+        foreach ($sortedProjects[1] as $project) {
+            $oldContent[] = displayProject($project);
+        }
+    }
     
     //donations
     if ($sortedDonations[1]) {
         foreach ($sortedDonations[1] as $donation) {
-            //output html
-            ?>
-            <div class='entry'>
-                <br />
-                <p>
-                    <a class='entry-title' 
-                        href='/Flint/?controller=pages&action=user&author=<?php 
-                        echo $donation->username; ?>'>
-                        <?php echo $donation->username; ?>
-                    </a>
-                    pledged $<?php echo $donation->amount; ?> towards 
-                    <a class='entry-title' 
-                        href='/Flint/?controller=pages&action=project&pid=<?php 
-                        echo $donation->pid; ?>'>
-                    <?php echo $donation->pname ?>
-                    </a>
-                    .
-                </p>
-            </div>
-            <?php
+            $oldContent[] = displayDonation($donation);
         }
     }
 
     //posts
     if ($sortedPosts[1]) {
         foreach ($sortedPosts[1] as $post) {
-            //output html
-            ?>
-            <div class='entry'>
-                <h4>
-                    <a class='entry-title' 
-                        href='/Flint/?controller=pages&action=user&author=<?php 
-                        echo $post->author; ?>'>
-                        <?php echo $post->author; ?>
-                    </a>
-                    > 
-                    <a class='entry-title' 
-                        href='/Flint/?controller=pages&action=project&pid=<?php 
-                        echo $post->pid; ?>'>
-                        <?php echo
-                            ($post->owner)
-                            ? $post->pname . ' (Update)'
-                            : $post->pname; 
-                        ?>
-                    </a>
-                </h4>
-                <p><?php echo $post->comment; ?></p>
-            </div>
-            <?php
+            $oldContent[] = displayPost($post);
         }
+    }
+
+    shuffle($oldContent);
+    foreach ($oldContent as $html) {
+        echo $html;
     }
 
 ?>
